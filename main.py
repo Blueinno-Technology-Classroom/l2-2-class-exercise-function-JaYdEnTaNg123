b6c2e70fe501b1ab5b736c30846e771d964279e5
JaYdEnTaNg123 <tangjayden999@gmail.com>
2024-01-27 12:02:17 +0800

    vvbought('hen')vbought('hen')vbought('hen')bought('hen')bought('hen')vbought('hen')

--- main.py
+++ main.py
@@ -4,13 +4,23 @@ Q1:
 '''
 
 # TODO: Write your code here
+def bought( name):
+   print(f'Hi, {name}!')
 
+bought('Alice')
+bought('Bob')
 ##################################################
 '''
 Q2:
 '''
 
 # TODO: Write your code here
+def bought(name1, name2):
+   print(f'Hi, {name1} and {name2}!')
+
+
+bought(name2='Bob', name1='Alex')
+
 
 ##################################################
 '''
@@ -18,14 +28,23 @@ Q3:
 '''
 
 # TODO: Write your code here
+def find_rect_area(width, height):
+    print(width*height)
 
+find_rect_area(width=int(input()), height=int(input()))
 ##################################################
 '''
 Q4:
 '''
 
 # TODO: Write your code here
+def bought(animal):
+   print(f'Bought me a {animal} and the {animal} pleased me, I fed my {animal} under yonder tree. Cat goes fiddle-i-fee!')
 
+bought('cat')
+bought('hen')
+bought('duck')
+bought('goose')
 ##################################################
 '''
 Q5:
